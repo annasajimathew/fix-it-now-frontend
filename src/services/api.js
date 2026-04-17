67a5a6f59cd44fb1d7fc038bb77910ab3bd19be9
Anna Saji Mathew <98905184+annasajimathew@users.noreply.github.com>
2026-04-17 14:28:56 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://fix-it-now-server.vercel.app/",
 });
 
 //  attach token automatically
